71f48a2db33a441f9647296a7fb1b128c45e2d82
jmiller <jmiller@QG245990.qg.com>
2014-11-05 17:02:16 -0600

    initial commit

---
+++ src/randommessagegenerator/RandomMessageGenerator.java
@@ -0,0 +1,65 @@
+package randommessagegenerator;
+
+import java.util.*;
+
+/**
+ *
+ * @author jmiller
+ */
+public class RandomMessageGenerator {
+    
+    private List<String> list = new ArrayList<>();
+    private String message;
+    
+    public RandomMessageGenerator() {
+        list.add("\"Show me a good loser, and I'll show you a loser.\" -- Vince Lombardi");
+        list.add("\"From Sharp minds come... pointed heads.\" -- Bryan Sparrowhawk");
+        list.add("\"Whoever undertakes to set himself up as a judge of Truth and Knowledge is shipwrecked by the laughter of the gods.\" -- Albert Einstein ");
+        list.add("\"Engineering without management is art.\" -- Jeff Johnson");
+        list.add("\"There is nothing new under the sun, but there are lots of old things we don't know yet.\" -Ambrose Bierce ");
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this.message);
+        return hash;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RandomMessageGenerator other = (RandomMessageGenerator) obj;
+        if (!Objects.equals(this.message, other.message)) {
+            return false;
+        }
+        return true;
+    }
+    
+    public String getRandomMessage() {
+        Random random = new Random(System.nanoTime());
+        int i = random.nextInt(list.size());
+        return list.get(i);
+    }
+    
+    public void addMessage(String message) {
+        list.add(message);
+        removeDuplicates();
+    }
+    
+    public void removeDuplicates() {
+        Set<String> set = new HashSet<>(list);
+        list = new ArrayList<>(set);
+    }
+    
+    public int getListCount() {
+        int i = list.size();
+        return i;
+    }
+    
+}
---
+++ src/randommessagegenerator/Startup.java
@@ -0,0 +1,18 @@
+package randommessagegenerator;
+
+/**
+ *
+ * @author jmiller
+ */
+public class Startup {
+
+    public static void main(String[] args) {
+        RandomMessageGenerator randomMessageGenerator = new RandomMessageGenerator();
+        System.out.println(randomMessageGenerator.getRandomMessage());
+        System.out.println(randomMessageGenerator.getListCount());
+        randomMessageGenerator.addMessage("\"A great many people think they are thinking when they are merely rearranging their prejudices.\" -- William James"); 
+        System.out.println(randomMessageGenerator.getListCount());
+        randomMessageGenerator.addMessage("\"A great many people think they are thinking when they are merely rearranging their prejudices.\" -- William James"); 
+        System.out.println(randomMessageGenerator.getListCount());
+    }
+}
